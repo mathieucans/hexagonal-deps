6ad257292b2f7300419aad24199ac0d9d250fb11
mathieu cans <mathieucans@gmail.com>
2022-05-30 16:52:37 +0200

    Example of multiple dependencies on directory

--- console-application/src/index.ts
+++ console-application/src/index.ts
@@ -1,1 +1,20 @@
+import {Application} from "hexagonal.domain";
+import {UserRepositoryAdapter} from "hexagonal.storage/lib/UserRepositoryAdapter";
+
 console.log('application start');
+
+class ConsoleApplication{
+    async start() {
+        let application = new Application(new UserRepositoryAdapter());
+
+        console.log("add users")
+        await application.addUser('me')
+        await application.addUser('you')
+
+        console.log("say hello");
+        let hello = await application.hello();
+        console.log(` > ${hello}`)
+    }
+}
+
+new ConsoleApplication().start();
